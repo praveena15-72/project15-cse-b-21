f58b56bf8cbf242a41ddd12b8063cd49a56e1a73
praveena15-72 <62324543+praveena15-72@users.noreply.github.com>
2021-04-16 13:23:36 +0530

    Add files via upload

--- script.aes.py
+++ script.aes.py
@@ -52,3 +52,62 @@ class Encryptor:
                 if (fname != 'script.py' and fname != 'data.txt.enc'):
                     dirs.append(dirName + "\\" + fname)
         return dirs
+        def encrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.encrypt_file(file_name)
+
+    def decrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.decrypt_file(file_name)
+
+
+key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
+enc = Encryptor(key)
+clear = lambda: os.system('cls')
+
+if os.path.isfile('data.txt.enc'):
+    while True:
+        password = str(input("Enter password: "))
+        enc.decrypt_file("data.txt.enc")
+        p = ''
+        with open("data.txt", "r") as f:
+            p = f.readlines()
+        if p[0] == password:
+            enc.encrypt_file("data.txt")
+            break
+
+    while True:
+        clear()
+        choice = int(input(
+            "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
+        clear()
+        if choice == 1:
+            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+        elif choice == 2:
+            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+        elif choice == 3:
+            enc.encrypt_all_files()
+        elif choice == 4:
+            enc.decrypt_all_files()
+        elif choice == 5:
+            exit()
+        else:
+            print("Please select a valid option!")
+
+else:
+    while True:
+        clear()
+        password = str(input("Setting up stuff. Enter a password that will be used for decryption: "))
+        repassword = str(input("Confirm password: "))
+        if password == repassword:
+            break
+        else:
+            print("Passwords Mismatched!")
+    f = open("data.txt", "w+")
+    f.write(password)
+    f.close()
+    enc.encrypt_file("data.txt")
+    print("Please restart the program to complete the setup")
+    time.sleep(15)
